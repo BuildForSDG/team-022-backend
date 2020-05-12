45b256931282f8807c78eeea95813e7f69c83ee5
Tundeh <jamiu4freedom@gmail.com>
2020-05-12 02:29:08 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
-const app = async () => '#BuildforSDG';
-
-export default app;
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const db = require('./config/keys');
+mongoose
+  .connect(db.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('Successfully connected to MongoDB Atlas! '))
+  .catch((error) => console.log(error));
+const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+app.use(bodyParser.json());
+app.use('/', (req, res) => {
+  res.json({ message: 'connection successful' });
+});
+module.exports = app;
---
+++ src/config/keys.js
@@ -0,0 +1,6 @@
+const dotenv = require('dotenv');
+require('dotenv').config();
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
+};
