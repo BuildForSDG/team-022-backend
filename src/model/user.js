cfdc13b72bba59ddcc1f0a77ed9b3be835dd5f11
Tundeh <jamiu4freedom@gmail.com>
2020-05-15 21:33:02 +0100

    add sign up route and controller #2

--- src/model/user.js
+++ src/model/user.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: email, required: true },
-  passhash: { type: String, required: true },
-  age: { type: Number, required: true }
+  passHash: { type: String, required: true },
+  state: { type: String, required: true },
+  country: { type: String, required: true },
+  age: { type: Number, required: true },
+  maritalStatus: { type: String, enum: ['single', 'married'], required: true },
+  sex: { type: String, required: true },
+  sexOrientation: { type: String, required: true },
+  religious: { type: String, enum: ['yes', 'no'], required: true },
+  religion: { type: String, required: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
